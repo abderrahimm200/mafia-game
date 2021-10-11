aac7392efe45bcdd29f3cb0e1df2f7ac1a395457
abderrahimm200 <65556094+abderrahimm200@users.noreply.github.com>
2021-10-11 13:29:09 +0200

    Update create.py

--- create.py
+++ create.py
@@ -11,7 +11,7 @@ app.config['SECRET_KEY']='mafiamafiagame'
 uri = os.getenv("DATABASE_URL")
 if uri and uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
-app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI']=uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key=os.environ.get("SECRET")
 socketio = SocketIO(app)
